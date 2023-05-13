dcb65e5dbe375745edf52ea2824884c89c1d8818
ngalinos95 <ngalinos95@me.com>
2023-05-13 23:40:14 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Nikos Galinos on 13/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
